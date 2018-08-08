f483454fc4637d85528885a7bfb6e1f5d656f895
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-08 13:45:54 -0400

    Got all the shapes drawing

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const MAX = 400
-let container = document.getElementById("draw")
+//global variables and functions
+const MAX = 400;
+let container = document.getElementById("draw");
+function randomVal (min,max){
+    return Math.floor(Math.random() * (max-min) + min)
+};
 
 class Shape {
-    constructor(x,y){
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
         this.div = document.createElement('div');
-        this.div.classList.add('square')
-        this.div.style.left = `${x}px`;
-        this.div.style.top = `${y}px`;
+        this.div.classList.add('square');
         this.updateColor();
-        container.appendChild(this.div)
+        this.draw();
     }
 
-
+    draw() {
+        container.appendChild(this.div);
+        let x = randomVal(0,MAX);
+        let y = randomVal(0,MAX);
+        this.div.style.height = `${this.height}px`;
+        this.div.style.width = `${this.width}px`;
+        this.div.style.left = `${x}px`;
+        this.div.style.top = `${y}px`;
+    }
 
     updateColor(){
         let randmColor = `rgb(${randomVal(0,255)},${randomVal(0,255)}, ${randomVal(0,255)}`
@@ -19,51 +31,77 @@ class Shape {
     }
 }
 
+class Rectangle extends Shape {
+    constructor(height, width) {
+        super(height, width);
+    }
+}
 
-function insertSquare(){
-    let xVal = randomVal(0,MAX)
-    let yVal = randomVal(0,MAX)
-    let oh = new Shape(xVal,yVal)
-   
+class Square extends Shape {
+    constructor(pizza) {
+        super(pizza, pizza);
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius){
+        super(radius,radius);
+        let border = radius/2;
+        this.div.style.borderRadius = `${border}px`;
+        
+    }
+}
+
+class Triangle extends Shape {
+    constructor(Height){
+        super(0,0);
+        let border = Height*Height/2;
+        this.div.style.borderBottom = `${border}px solid red`;
+        this.div.style.borderLeft = `${border}px solid transparent`;
+        this.div.style.backgroundColor = `transparent`
+
+    }
 }
 
 
-function randomVal (min,max){
-    return Math.floor(Math.random() * (max-min) + min)
-}
-
-//Event Handlers
-
-
+//RECTANGLE
 document.getElementById("rec").addEventListener("click", function () {
     
+    let heighInput = document.getElementById("rec-height");
+    let widthInput = document.getElementById("rec-width");
+
+    let height = heighInput.value;
+    let width = widthInput.value;
+
+    let rectangle = new Rectangle(height, width);
+
+    //ghetto console log
     let t = document.createTextNode(`${"This is a rectangle"}`)
     document.getElementById("side").appendChild(t)
 });
-
+//SQUARE
 document.getElementById("sq").addEventListener("click", function () {   
-    insertSquare()
+    
+    let sideInput = document.getElementById("sq-side");
+
+    let sideLength = sideInput.value;
+
+    let square = new Square(sideLength);
+
+    //ghetto console log
     let t = document.createTextNode(`${"This is a square"}`)
     document.getElementById("side").appendChild(t)
 });
+//CIRCLE
+document.getElementById("circ").addEventListener("click", function (){
+    let radiusInput = document.getElementById("radius");
+    let  radius = radiusInput.value;
+    let c = new Circle(radius)
 
-
-
-document.getElementById("circ").addEventListener("click", function () {
-    let t = document.createTextNode(`${"This is a circle"}`)
-    document.getElementById("side").appendChild(t)
-   
-});
-
-
-document.getElementById("tri").addEventListener("click", function () {
-    let t = document.createTextNode(`${"This is a triangle"}`)
-    document.getElementById("side").appendChild(t)    
-});
-
-
-
-
-
-
-
+})
+//Triangle
+document.getElementById("tri").addEventListener('click', function() {
+    let triHeightInput = document.getElementById('tri-height')
+    let triHeight = triHeightInput.value;
+    let t = new Triangle(triHeight)
+})
